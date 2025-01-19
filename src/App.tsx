fcdb3acae02258c9a23869defafb71440ce3abb7
Isaac <mayne.isaac@gmail.com>
2025-01-19 22:51:27 +0100

    add first css and font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-      <h1>Cripto App</h1>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+        <div className="content">
+          
+        </div>
+      </div>
     </>
   )
 }
